7edbad2621243d2a327f6f692ddb6214e9976fb7
Orest-Neiming <ygrinom@gmail.com>
2024-06-20 11:11:06 +0200

    gggg commit

---
+++ lecture0/homework/task3/circle.java
@@ -0,0 +1,9 @@
+package sk.com.ymca.javaStart.lecture0.homework.task3;
+
+public class circle {
+    public static void main(String[] args) {
+        final double PI = Math.PI;
+        int r = 30;
+        System.out.println(PI * Math.pow(r, 2));
+    }
+}
