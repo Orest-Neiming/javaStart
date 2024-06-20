640ca38fb8c33c6e70d45504da26eda6adb675d4
Orest-Neiming <ygrinom@gmail.com>
2024-06-20 11:48:29 +0200

    test commit number three hundred

--- lecture0/homework/task1/main.java
+++ lecture0/homework/task1/main.java
@@ -15,6 +15,7 @@ public class main {
         int penis8 = x /= x + x;
         int penis9 = x = (x) + x;
         int penis10 = 20;
+        int penis12 = 10;
         // ghhghg
         System.out.println(penis + "\n" + penis2 + "\n" + penis3 + "\n" + penis4 + "\n" + penis5 + "\n" + penis6 + "\n" + penis7 + "\n" + penis8 + "\n" + penis9);
         System.out.println(penis10);
