be4aafdcd094dc736c2672f8d620a63722f7d581
Orest-Neiming <ygrinom@gmail.com>
2024-06-21 10:11:31 +0200

    ttttttttttj

--- lecture1/homework/task1/Test.java
+++
@@ -1,9 +0,0 @@
-package sk.com.ymca.javaStart.lecture1.homework.task1;
-
-public class Test {
-    public static void main(String[] args) {
-        if (3 > 2) {
-            System.out.println("hello");
-        }
-    }
-}
